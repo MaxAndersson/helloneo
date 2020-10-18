966c14c6dda8f4b485dfe27dad5b47d83482b514
MaxAndersson <maxn90@gmail.com>
2020-10-19 00:46:09 +0200

    cleanup

--- src/authorizer/app.js
+++ src/authorizer/app.js
@@ -1,6 +1,4 @@
 var jwt = require('jsonwebtoken');
-var AWS = require('aws-sdk');
-let response;
 // AWS.config.update({region: 'eu-central-1'});
 // let ssm = new AWS.SSM({});
 // let jwtSecret;
@@ -17,30 +15,6 @@ let response;
  * @returns {Object} object - API Gateway Lambda Proxy Output Format
  * 
  */
-// exports.lambdaHandler = async (event, context) => {
-//     try {
-//         let token  = event.authorizeationToken.split(' ')[1]
-//         let secret = await getParameters()
-//             const decodedToken =  jwt.verify(token, secret, function(err, decodedToken) {
-//                 if (err) console.log(err, err.stack); // an error occurred
-//                 else {
-//                     if (decodedToken.database == 'movies'){
-//                     context.succeed(generatePolicy('user', 'Allow', event.MethodArn));
-//                     }else {
-//                         context.fail('Unauthorized')
-//                     }
-//                 }
-                
-//             });
-      
-//     } catch (err) {
-//         console.log(err);
-//         return err;
-//     }
-
-//     return response
-// };
-
 exports.token = async (event,context) => {
     try {
             const secret = process.env.JWT_SECRET
@@ -54,45 +28,3 @@ exports.token = async (event,context) => {
         console.log(err)
     }
 }
-
-// const   getParameters =  async() => {
-//     if (jwtSecret == undefined){
-
-//         let params = {
-//             Name: "JWT_SECRET",
-//             WithDecryption: true
-//         };
-//         try {
-//                 jwtSecret =  await ssm.getParameter(params).promise()
-//                 return jwtSecret.Parameter.Value
-            
-//         } catch (error) {
-//             console.log(error)
-            
-//         }
-       
-            
-//     }else {
-//       return jwtSecret.Parameter.Value
-//     }
-   
-// }
-
-// var generatePolicy = function(principalId, effect, resource) {
-//     var authResponse = {};
-    
-//     authResponse.principalId = principalId;
-//     if (effect && resource) {
-//         var policyDocument = {};
-//         policyDocument.Version = '2012-10-17'; 
-//         policyDocument.Statement = [];
-//         var statementOne = {};
-//         statementOne.Action = 'execute-api:Invoke'; 
-//         statementOne.Effect = effect;
-//         statementOne.Resource = resource;
-//         policyDocument.Statement[0] = statementOne;
-//         authResponse.policyDocument = policyDocument;
-//     }
-    
-//     return authResponse;
-// }
